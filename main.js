24eac73d83aad20aaafc994381eb797d837044d8
alekoprograsmis <135098815+alekoprograsmis@users.noreply.github.com>
2023-12-28 19:28:30 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+var red = document.querySelector('.red')
+var orange = document.querySelector('.orange')
+var blue = document.querySelector('.blue')
+var aqua = document.querySelector('.aqua')
+var bg = document.querySelector('body')
+red.addEventListener('click',() =>{
+   bg.style.background = 'red'
+
+})
+orange.addEventListener('click',() =>{
+    bg.style.background = 'orange'
+ 
+ })
+ blue.addEventListener('click',() =>{
+    bg.style.background = 'blue'
+ 
+ })
+ aqua.addEventListener('click',() =>{
+    bg.style.background = 'aqua'
+ 
+ })
+
+
